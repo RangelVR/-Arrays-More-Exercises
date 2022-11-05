f96fbaa69729b28d6152b30ac97cff5c3f667735
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2022-11-05 20:43:44 +0200

    Update Program.cs

--- 01. Encrypt, Sort and Print Array/Program.cs
+++ 01. Encrypt, Sort and Print Array/Program.cs
@@ -8,7 +8,6 @@ namespace _01._Encrypt__Sort_and_Print_Array
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-
             int[] output = new int[n];
 
             for (int i = 0; i < n; i++)
