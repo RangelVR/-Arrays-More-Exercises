9620f097dc52e05d61e2f10c809e88b001c2c2fd
Rangel Rangelov <99962525+RangelVR@users.noreply.github.com>
2023-02-01 22:53:09 +0200

    Update Program.cs

--- 03. Recursive Fibonacci/Program.cs
+++ 03. Recursive Fibonacci/Program.cs
@@ -1,22 +1,24 @@
-using System;
-using System.Linq;
 
-namespace LongestIncreasingSubsequence
+int fibunacciNum = int.Parse(Console.ReadLine());
+long[] result = new long[50];
+result[0] = 1;
+result[1] = 1;
+
+for (int i = 2; i < fibunacciNum; i++)
 {
-    class Program
-    {
-        public static void Main()
-        {
-            int fibunacciNum = int.Parse(Console.ReadLine());
-            long[] result = new long[50];
-            result[0] = 1;
-            result[1] = 1;
-
-            for (int i = 2; i < fibunacciNum; i++)
-            {
-                result[i] = result[i - 1] + result[i - 2];
-            }
-            Console.WriteLine(result[fibunacciNum - 1]);
-        }
-    }
+    result[i] = result[i - 1] + result[i - 2];
 }
+Console.WriteLine(result[fibunacciNum - 1]);
+
+
+//int n = int.Parse(Console.ReadLine());
+//int[] arr = new int[50];
+//arr[0] = 1;
+//arr[1] = 1;
+
+//for (int i = 0; i < n - 2; i++)
+//{
+//    arr[i + 2] = arr[i] + arr[i + 1];
+//}
+
+//Console.WriteLine(arr[n - 1]);
